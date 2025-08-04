a37ef7eb18cc546f4c6bd7cbe1f0ed21c46a7c02
Xiaowei Tan <39715117+xw-TAN@users.noreply.github.com>
2025-08-04 18:09:37 +0100

    Add files via upload
    script to estimate shank 3D orientation angles based on three markers' trajectory (Shank_Upper, Shank_Med, Shank_Lat).

---
+++ Edinburgh/ToeClearanceEstimation/calc3DShankOrientation.m
@@ -0,0 +1,60 @@
+function [euler_angles] = calc3DShankOrientation(s_upp, s_med, s_lat, side)
+% input:
+%   s_upp: shank_upper, Nx3，N frames and 3D coodinates
+%   s_med: shank_med, Nx3
+%   s_lat: shank_lat, Nx3
+%   side: 1 char, 'L' or 'R', naming the leg side the markers placed on
+%
+% output：
+%   euler_angles: Nx3，N frames (deg)
+%
+
+N = size(s_upp, 1);
+R_all = zeros(3,3,N);
+
+%% create marker-based frame
+for i = 1:N
+    if side == 'L'
+        % x-axis, left direction whether the left or right leg
+        X = s_lat(i,:) - s_med(i,:);
+        X = X / norm(X);
+    elseif side == 'R'
+        X = s_med(i,:) - s_lat(i,:);
+        X = X / norm(X);
+    else
+        disp('side value input error');
+    end
+   
+    % midpoint
+    mid_front = (s_med(i,:) + s_lat(i,:)) / 2;
+
+    temp = cross(X, s_upp(i,:) - mid_front); % z-axis, forward direction
+    Z = temp / norm(temp);
+    
+    % y-axis, upward direction
+    Y = cross(Z, X);    
+    Y = Y / norm(Y);
+
+    % rotationay matrix of marker-based frame relative to Vicon world frame
+    % the code below ensures it represents the relation of Marker to Vicon
+    R_all(:,:,i) = [X' Y' Z'];
+end
+
+
+%% calculate angles of marker-based frame to the Vicon frame
+
+euler_angles = zeros(N,3);
+for i = 1:N
+    % representation of Marker frame relative to Vicon frame means to 
+    % rotate Vicon frame to get Marker frame (From_Vicon_to_Marker)
+    R_From_Vicon_to_Marker = R_all(:,:,i);
+    
+    % convert to euler angles (deg). Don't know why ZXY shows best results
+    euler_angles(i,:) = rad2deg(rotm2eul(R_From_Vicon_to_Marker, 'ZXY'));
+end
+
+% avoid jumps in angle
+euler_angles = unwrap(deg2rad(euler_angles), [], 1) * 180/pi;
+
+end
+
